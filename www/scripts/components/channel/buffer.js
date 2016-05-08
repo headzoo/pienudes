969c18811cef0c0b9a654080bc9beb6219614220
headzoo <sean@headzoo.io>
2016-05-07 23:24:23 -0400

    Saving

--- www/scripts/components/channel/buffer.js
+++ www/scripts/components/channel/buffer.js
@@ -12,32 +12,32 @@ var Component = React.createClass({
     ],
     
     componentDidMount: function() {
-        /*
-        $("#channel-buffer").mCustomScrollbar({
+        $(this.refs.buffer).mCustomScrollbar({
             theme: "minimal",
             autoHideScrollbar: true,
             mouseWheel: {
                 scrollAmount: 300
             }
-        }).mCustomScrollbar("scrollTo", "bottom");
-        */
+        });
     },
     
     componentDidUpdate: function() {
-        /*
-        $("#channel-buffer")
+        $(this.refs.buffer)
             .mCustomScrollbar("update")
             .mCustomScrollbar("scrollTo", "bottom");
-            */
     },
     
     render: function () {
         return (
             <div id="channel-buffer-wrap" className="col-xs-12 col-sm-7 col-md-5">
-                <div id="channel-buffer">
-                    {this.state.messages.map(function(m, i) {
-                        return <Message key={i} data={m} />
-                    })}
+                <div ref="buffer" id="channel-buffer">
+                    <div id="mCSB_1" className="mCustomScrollBox mCS-minimal mCSB_vertical mCSB_outside" style={{maxHeight: "none"}} tabIndex="0">
+                        <div id="mCSB_1_container" className="mCSB_container mCS_y_hidden mCS_no_scrollbar_y" style={{position:"relative", top:0, left:0}} dir="ltr">
+                            {this.state.messages.map(function(m, i) {
+                                return <Message key={i} data={m} />
+                            })}
+                        </div>
+                    </div>
                 </div>
                 <Input />
             </div>
