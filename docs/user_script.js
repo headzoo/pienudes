c704785fb160b0c4437ae52676fc57f07a77a858
headzoo <sean@headzoo.io>
2016-06-09 21:50:33 -0400

    Updated to scripting docs

--- docs/user_script.js
+++ docs/user_script.js
@@ -1,12 +1,13 @@
+// The "favorites" event is called when you enter chat. Save the favorites to a local variable.
 var favorites = [];
-
 $api.on("favorites", function(e, data) {
     favorites = data;
 });
 
+// Every 30 minutes, grab one of the favorites at random, and add it to the playlist.
 setInterval(function() {
     var item = favorites[Math.floor(Math.random() * favorites.length)];
     if (item) {
         $api.queue(item);
     }
-}, 30 * 60 * 1000); // 30 minutes
+}, 1800000); // 30 minutes in milliseconds
