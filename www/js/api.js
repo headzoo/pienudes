e7758b376c3329c202805cf59ba39e159752c3f6
headzoo <sean@headzoo.io>
2016-06-22 16:00:23 -0400

    api update

--- www/js/api.js
+++ www/js/api.js
@@ -481,7 +481,8 @@ var $each = function(obj, cb) {
             toastr.options.preventDuplicates = true;
             toastr.options.closeButton = true;
             toastr.options.timeOut = (time_out || 1500);
-            
+    
+            msg = jQuery(msg).text();
             switch(type) {
                 case "warning":
                     toastr.warning(msg);
