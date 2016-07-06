ebdd88f773a2308a0d866cb01562d65dcdc80409
headzoo <sean@headzoo.io>
2016-07-06 11:16:56 -0400

    rngmod > 1 vote

--- src/database/playlist.js
+++ src/database/playlist.js
@@ -312,7 +312,7 @@ module.exports = {
                 "INNER JOIN `votes` ON `votes`.`media_id` = `media`.`id` " +
                 "WHERE `channel` = ? " +
                 "GROUP BY `media`.`id` " +
-                "HAVING SUM(`votes`.`value`) > 0 " +
+                "HAVING SUM(`votes`.`value`) > 1 " +
                 "ORDER BY RAND() " +
                 "LIMIT " + limit;
         } else {
@@ -322,7 +322,7 @@ module.exports = {
                 "WHERE `channel` = ? " +
                 "AND FROM_UNIXTIME(`playlist_history`.`time` / 1000) > DATE_SUB(NOW(), INTERVAL 2 WEEK)" +
                 "GROUP BY `media`.`id` " +
-                "HAVING SUM(`votes`.`value`) > 0 " +
+                "HAVING SUM(`votes`.`value`) > 1 " +
                 "ORDER BY RAND() " +
                 "LIMIT " + limit;
         }
